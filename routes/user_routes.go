bcc9135189b356880fc1ef39efc630b7c4849e4b
habibunayka <habibunayka@gmail.com>
2025-04-07 20:42:51 +0700

    Add Register and Postman API Test

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -12,12 +12,12 @@ func InitUserRoutes(r *gin.RouterGroup, ctrl *controller.AllController) {
 	authMiddleware := middleware.NewMiddleware()
 
 	r.POST("/user/login", ctrl.UserController.Login)
+	r.POST("/user/register", ctrl.UserController.CreateUser)
 
 	userGroup := r.Group("/user",  authMiddleware.AuthMiddleware())
 	{
 		userGroup.GET("/", ctrl.UserController.GetAllUser)
 		userGroup.GET("/:id", ctrl.UserController.GetUserByID)
-		userGroup.POST("/", ctrl.UserController.CreateUser)
 		userGroup.PUT("/:id", ctrl.UserController.UpdateUser)
 		userGroup.DELETE("/:id", ctrl.UserController.DeleteUser)
 		userGroup.DELETE("/:id/force", ctrl.UserController.ForceDeleteUser)
