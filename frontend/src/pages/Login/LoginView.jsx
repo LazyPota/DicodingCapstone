80be949112436043e4eed88f955791ef10c57b6c
Fahminur12 <fahmisenp@gmail.com>
2025-04-10 12:14:30 +0700

    Adding Validation Form in Login and Register Page

--- frontend/src/pages/Login/LoginView.jsx
+++ frontend/src/pages/Login/LoginView.jsx
@@ -2,8 +2,18 @@ import React from "react"; // Tidak perlu useState jika hanya digunakan di kompo
 import logo from "../../assets/logoFIX.png";
 import container from "../../assets/container.png";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { Link } from "react-router-dom";
 
-const LoginView = ({ setShowPassword, showPassword }) => {
+const LoginView = ({
+  setShowPassword,
+  showPassword,
+  handleSubmit,
+  errors,
+  email,
+  setEmail,
+  password,
+  setPassword,
+}) => {
   return (
     <main className="relative flex min-h-[140vh] md:p-2">
       <header className="absolute top-4 left-4">
@@ -22,40 +32,94 @@ const LoginView = ({ setShowPassword, showPassword }) => {
             id="login-heading"
             className="text-3xl md:text-[40px] font-bold font-inter text-center lg:text-left"
           >
-            Login
+            Masuk
           </h1>
           <p className="text-[18px] font-normal font-inter text-[#969696] mt-2 text-center lg:text-left">
-            Please login to continue to your account.
+            Silakan masuk untuk melanjutkan akun Anda.
           </p>
-          <form className="mt-3 flex flex-col space-y-[20px]">
+          <form
+            className="mt-3 flex flex-col space-y-[20px]"
+            onSubmit={handleSubmit}
+            noValidate
+          >
             <div className="relative">
               <input
                 type="email"
-                id="email_input"
-                className="block px-2.5 pb-2.5 pt-4 w-full h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border border-[#D9D9D9] appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                id="email"
+                name="email"
+                className={`block px-2.5 pb-2.5 pt-4 w-full h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border ${
+                  errors.email
+                    ? "border-red-500 focus:border-red-500"
+                    : "border-[D9D9D9]"
+                } appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer`}
                 placeholder=" "
+                value={email}
                 required
                 aria-required="true"
+                onChange={(e) => setEmail(e.target.value)}
+                aria-invalid={errors.email ? "true" : "false"}
+                aria-describedby={errors.email ? "email-error" : undefined}
               />
               <label
-                htmlFor="default_outlined"
-                className="absolute text-[18px] font-medium font-inter text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[14px]"
+                htmlFor="email"
+                className={`
+                  absolute text-[18px] font-medium font-inter duration-300 transform z-10 origin-[0] bg-white px-2 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto
+                  ${
+                    errors.email
+                      ? "-translate-y-4 scale-75 top-2"
+                      : "peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 -translate-y-4 scale-75 top-2"
+                  }
+                  peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:px-2 peer-focus:text-[14px]
+                  ${
+                    errors.email
+                      ? "text-red-600 peer-focus:text-red-600"
+                      : "text-gray-500 peer-focus:text-blue-600"
+                  }
+                `}
               >
                 Email
               </label>
+              {errors.email && (
+                <p id="email-error" className="mt-1 text-xs text-red-500">
+                  {errors.email}
+                </p>
+              )}
             </div>
             <div className="relative">
               <input
                 type={showPassword ? "text" : "password"}
-                id="password_input"
-                className="block px-2.5 pb-2.5 pt-4 w-full h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border border-[#D9D9D9] appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer pr-10"
+                id="password"
+                name="password"
+                className={`block px-2.5 pb-2.5 pt-4 w-full h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border ${
+                  errors.password
+                    ? "border-red-500 focus:border-red-500 "
+                    : "border-[#D9D9D9]"
+                } appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer pr-10`}
                 placeholder=" "
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 required
                 aria-required="true"
+                aria-invalid={errors.password ? "true" : "false"}
+                aria-describedby={
+                  errors.password ? "password-error" : undefined
+                }
               />
               <label
-                htmlFor="password_input"
-                className="absolute text-[18px] font-medium font-inter text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[14px]"
+                htmlFor="password"
+                className={`absolute text-[18px] font-medium font-inter duration-300 transform z-10 origin-[0] bg-white px-2 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto
+                ${
+                  errors.password
+                    ? "-translate-y-4 scale-75 top-2"
+                    : "peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 -translate-y-4 scale-75 top-2"
+                }
+                peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:px-2 peer-focus:text-[14px]
+                ${
+                  errors.password
+                    ? "text-red-600 peer-focus:text-red-600"
+                    : "text-gray-500 peer-focus:text-blue-600"
+                }
+                `}
               >
                 Password
               </label>
@@ -63,7 +127,9 @@ const LoginView = ({ setShowPassword, showPassword }) => {
                 type="button"
                 className="absolute z-10 top-1/2 right-3 -translate-y-1/2 text-gray-500 cursor-pointer"
                 onClick={() => setShowPassword(!showPassword)}
-                aria-label={showPassword ? "Hide password" : "Show password"}
+                aria-label={
+                  showPassword ? "Sembunyikan password" : "Tampilkan password"
+                }
               >
                 {showPassword ? (
                   <Icon
@@ -75,54 +141,64 @@ const LoginView = ({ setShowPassword, showPassword }) => {
                   <Icon icon="mdi:eye" className="w-5 h-5" aria-hidden="true" />
                 )}
               </button>
+              {errors.password && (
+                <p id="password-error" className="mt-1 text-xs text-red-500">
+                  {errors.password}
+                </p>
+              )}
             </div>
-            <div className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                id="keep_logged_in"
-                className="w-[18px] h-[18px] border border-black rounded focus:ring-2 focus:ring-blue-500"
-              />
-              <label
-                htmlFor="keep_logged_in"
-                className="text-[16px] font-medium text-gray-900 font-inter"
-              >
-                Keep me logged in
-              </label>
+            <div className="flex flex-row justify-between items-center">
+              <div className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  id="keep_logged_in"
+                  className="w-[18px] h-[18px] border border-black rounded focus:ring-2 focus:ring-blue-500"
+                />
+                <label
+                  htmlFor="keep_logged_in"
+                  className="text-[16px] font-medium text-gray-900 font-inter"
+                >
+                  Biarkan saya tetap masuk
+                </label>
+              </div>
+              <Link className="text-blue-500 font-inter font-semibold text-sm underline hover:text-blue-700 transition-colors delay-75">
+                Lupa Kata Sandi
+              </Link>
             </div>
             <div className="flex flex-col items-center space-y-4 w-full pt-4">
               <button
                 type="submit"
                 className="w-full font-inter h-[54px] bg-[#367AFF] text-white text-[18px] font-semibold rounded-[10px] hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-150 ease-in-out"
               >
-                Login
+                Masuk
               </button>
               <div className="flex items-center w-full">
                 <hr className="flex-grow border-t border-gray-300" />
                 <span className="px-3 text-gray-500 text-sm font-medium font-inter">
-                  or
+                  Atau
                 </span>
                 <hr className="flex-grow border-t border-gray-300" />
               </div>
               <button
                 type="button"
-                className="w-full h-[54px] border border-[#E6E8E7] rounded-[10px] flex items-center justify-center space-x-2 text-lg font-medium font-inter text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 transition duration-150 ease-in-out"
+                className="w-full h-[54px] border border-[#E6E8E7] rounded-[10px] flex items-center justify-center space-x-2 text-lg font-medium font-inter text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 transition duration-150 ease-in-out"
               >
                 <Icon
                   icon="flat-color-icons:google"
                   className="text-xl"
                   aria-hidden="true"
                 />
-                <span>Continue with Google</span>
+                <span>Masuk Dengan Google</span>
               </button>
             </div>
           </form>
           <p className="text-gray-500 text-[16px] font-medium font-inter text-center mt-[32px]">
-            Need an account?{" "}
+            Belum Punya Aku?{" "}
             <a
               href="/register"
               className="text-blue-600 font-semibold font-inter underline hover:text-blue-800"
             >
-              Create one
+              Daftar
             </a>
           </p>
         </div>
--- frontend/src/pages/Register/RegisterView.jsx
+++ frontend/src/pages/Register/RegisterView.jsx
@@ -8,11 +8,17 @@ import "react-datepicker/dist/react-datepicker.css";
 const RegisterView = ({
   setShowPassword,
   showPassword,
-  setSelectedDate,
-  selectedDate,
+  errors,
+  email,
+  password,
+  setEmail,
+  setPassword,
+  name,
+  setName,
+  handleSubmit,
 }) => {
   return (
-    <main className="relative flex min-h-[140vh] p-2 md:p-4 ">
+    <main className="relative flex min-h-[140vh] p-2">
       <header className="absolute top-4 left-4">
         <img
           src={logo}
@@ -30,88 +36,137 @@ const RegisterView = ({
             id="signup-heading"
             className="text-3xl md:text-[40px] font-bold font-inter text-center lg:text-left"
           >
-            Sign Up
+            Daftar
           </h1>
           <p className="text-[18px] font-normal font-inter text-[#969696] mt-2 text-center lg:text-left">
-            Sign up to enjoy the feature of Revolutie
+            Daftar untuk menikmati fitur Moneasy
           </p>
 
-          <form className="mt-3 flex flex-col space-y-[20px]">
+          <form
+            className="mt-3 flex flex-col space-y-[20px]"
+            onSubmit={handleSubmit}
+            noValidate
+          >
             <div className="relative">
               <input
                 type="text"
-                id="name_input"
-                className="block px-2.5 pb-2.5 pt-4 w-full h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border border-[#D9D9D9] appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                id="name"
+                name="name"
+                className={`block px-2.5 pb-2.5 pt-4 w-full h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border ${
+                  errors.password
+                    ? "border-red-500 focus:border-red-500 "
+                    : "border-[#D9D9D9]"
+                } appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer pr-10`}
                 placeholder=" "
+                value={name}
+                onChange={(e) => setName(e.target.value)}
                 required
                 aria-required="true"
+                aria-invalid={errors.name ? "true" : "false"}
+                aria-describedby={errors.name ? "name-error" : undefined}
               />
               <label
-                htmlFor="name_input"
-                className="absolute text-[18px] font-medium font-inter text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[14px]"
+                className={`
+                  absolute text-[18px] font-medium font-inter duration-300 transform z-10 origin-[0] bg-white px-2 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto
+                  ${
+                    errors.name
+                      ? "-translate-y-4 scale-75 top-2"
+                      : "peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 -translate-y-4 scale-75 top-2"
+                  }
+                  peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:px-2 peer-focus:text-[14px]
+                  ${
+                    errors.name
+                      ? "text-red-600 peer-focus:text-red-600"
+                      : "text-gray-500 peer-focus:text-blue-600"
+                  }
+                `}
               >
-                Your Name
+                Nama Lengkap
               </label>
+              {errors.name && (
+                <p id="email-error" className="mt-1 text-xs text-red-500">
+                  {errors.name}
+                </p>
+              )}
             </div>
-
-            <div className="relative">
-              <label
-                htmlFor="dob_input"
-                className="absolute font-inter font-medium text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus-within:px-2 peer-focus-within:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus-within:top-2 peer-focus-within:scale-75 peer-focus-within:-translate-y-4 start-1 rtl:peer-focus-within:translate-x-1/4 rtl:peer-focus-within:left-auto peer-focus-within:text-[14px]"
-              >
-                Date of Birth
-              </label>
-              <Icon
-                icon="mdi:calendar"
-                className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 w-5 h-5 pointer-events-none z-10"
-                aria-hidden="true"
-              />
-              <DatePicker
-                selected={selectedDate}
-                onChange={(date) => setSelectedDate(date)}
-                dateFormat="dd MMMM yyyy"
-                placeholderText=" "
-                id="dob_input"
-                className="peer block w-full pl-10 pr-2.5 pb-2.5 pt-4 font-medium h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border border-[#D9D9D9] appearance-none focus:outline-none focus:ring-0 focus:border-blue-600"
-                wrapperClassName="w-full" 
-                required
-                aria-required="true"
-                showYearDropdown
-                scrollableYearDropdown
-                yearDropdownItemNumber={50}
-                maxDate={new Date()}
-              />
-            </div>
-
             <div className="relative">
               <input
                 type="email"
-                id="email_input"
-                className="block px-2.5 pb-2.5 pt-4 w-full h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border border-[#D9D9D9] appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                id="email"
+                name="email"
+                className={`block px-2.5 pb-2.5 pt-4 w-full h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border ${
+                  errors.email
+                    ? "border-red-500 focus:border-red-500"
+                    : "border-[D9D9D9]"
+                } appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer`}
                 placeholder=" "
+                value={email}
                 required
                 aria-required="true"
+                onChange={(e) => setEmail(e.target.value)}
+                aria-invalid={errors.email ? "true" : "false"}
+                aria-describedby={errors.email ? "email-error" : undefined}
               />
               <label
-                htmlFor="email_input"
-                className="absolute text-[18px] font-medium text-gray-500 font-inter duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[14px]"
+                htmlFor="email"
+                className={`
+                  absolute text-[18px] font-medium font-inter duration-300 transform z-10 origin-[0] bg-white px-2 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto
+                  ${
+                    errors.email
+                      ? "-translate-y-4 scale-75 top-2"
+                      : "peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 -translate-y-4 scale-75 top-2"
+                  }
+                  peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:px-2 peer-focus:text-[14px]
+                  ${
+                    errors.email
+                      ? "text-red-600 peer-focus:text-red-600"
+                      : "text-gray-500 peer-focus:text-blue-600"
+                  }
+                `}
               >
                 Email
               </label>
+              {errors.email && (
+                <p id="email-error" className="mt-1 text-xs text-red-500">
+                  {errors.email}
+                </p>
+              )}
             </div>
-
             <div className="relative">
               <input
                 type={showPassword ? "text" : "password"}
-                id="password_input"
-                className="block px-2.5 pb-2.5 pt-4 w-full h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border border-[#D9D9D9] appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer pr-10"
+                id="password"
+                name="password"
+                className={`block px-2.5 pb-2.5 pt-4 w-full h-[59px] text-sm text-gray-900 bg-transparent rounded-[10px] border ${
+                  errors.password
+                    ? "border-red-500 focus:border-red-500 "
+                    : "border-[#D9D9D9]"
+                } appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer pr-10`}
                 placeholder=" "
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
                 required
                 aria-required="true"
+                aria-invalid={errors.password ? "true" : "false"}
+                aria-describedby={
+                  errors.password ? "password-error" : undefined
+                }
               />
               <label
-                htmlFor="password_input"
-                className="absolute text-[18px] font-medium font-inter text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-[14px]"
+                htmlFor="password"
+                className={`absolute text-[18px] font-medium font-inter duration-300 transform z-10 origin-[0] bg-white px-2 start-1 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto
+                ${
+                  errors.password
+                    ? "-translate-y-4 scale-75 top-2"
+                    : "peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 -translate-y-4 scale-75 top-2"
+                }
+                peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:px-2 peer-focus:text-[14px]
+                ${
+                  errors.password
+                    ? "text-red-600 peer-focus:text-red-600"
+                    : "text-gray-500 peer-focus:text-blue-600"
+                }
+                `}
               >
                 Password
               </label>
@@ -122,11 +177,20 @@ const RegisterView = ({
                 aria-label={showPassword ? "Hide password" : "Show password"}
               >
                 {showPassword ? (
-                  <Icon icon="mdi:eye-off" className="w-5 h-5" aria-hidden="true" />
+                  <Icon
+                    icon="mdi:eye-off"
+                    className="w-5 h-5"
+                    aria-hidden="true"
+                  />
                 ) : (
                   <Icon icon="mdi:eye" className="w-5 h-5" aria-hidden="true" />
                 )}
               </button>
+              {errors.password && (
+                <p id="email-error" className="mt-1 text-xs text-red-500">
+                  {errors.password}
+                </p>
+              )}
             </div>
 
             <div className="flex flex-col items-center space-y-4 w-full pt-4">
@@ -134,44 +198,51 @@ const RegisterView = ({
                 type="submit"
                 className="w-full font-inter h-[54px] bg-[#367AFF] text-white text-[18px] font-semibold rounded-[10px] hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-150 ease-in-out"
               >
-                Sign Up
+                Buat Akun
               </button>
 
               <div className="flex items-center w-full">
                 <hr className="flex-grow border-t border-gray-300" />
                 <span className="px-3 text-gray-500 text-sm font-medium font-inter">
-                  or
+                  Atau
                 </span>
                 <hr className="flex-grow border-t border-gray-300" />
               </div>
 
               <button
                 type="button"
-                className="w-full h-[54px] border border-[#E6E8E7] rounded-[10px] flex items-center justify-center space-x-2 text-lg font-medium font-inter text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 transition duration-150 ease-in-out"
+                className="w-full h-[54px] border border-[#E6E8E7] rounded-[10px] flex items-center justify-center space-x-2 text-lg font-medium font-inter text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 focus:ring-offset-2 transition duration-150 ease-in-out "
               >
-                <Icon icon="flat-color-icons:google" className="text-xl" aria-hidden="true"/>
-                <span>Continue with Google</span>
+                <Icon
+                  icon="flat-color-icons:google"
+                  className="text-xl"
+                  aria-hidden="true"
+                />
+                <span>Masuk Dengan Google</span>
               </button>
             </div>
           </form>
 
           <p className="text-gray-500 text-[16px] font-medium font-inter text-center mt-[32px]">
-            Already have an account?{" "}
+            Sudah Punya Akun?{" "}
             <a
               href="/login"
               className="text-blue-600 font-semibold font-inter underline hover:text-blue-800"
             >
-              Login
+              Masuk
             </a>
           </p>
         </div>
       </section>
 
-      <aside className="hidden lg:flex justify-center items-center w-1/2" aria-hidden="true">
+      <aside
+        className="hidden lg:flex justify-center items-center w-1/2"
+        aria-hidden="true"
+      >
         <img
           src={container}
           alt="Decorative container illustration"
-          className="w-full h-full object-cover"
+          className="w-full h-full object-cover rounded-[16px]"
         />
       </aside>
     </main>
