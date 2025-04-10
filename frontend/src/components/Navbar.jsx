5d63c48b2cbf87b2d5e08681ac5b2eef3d47bcee
Fahminur12 <fahmisenp@gmail.com>
2025-04-10 10:00:32 +0700

    Delete Button Register in Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import logo from "./../assets/logoFIX.png";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
@@ -15,23 +16,17 @@ const Navbar = () => {
         <li className="hover:text-blue-600 cursor-pointer">Testimoni</li>
         <li className="hover:text-blue-600 cursor-pointer">Hubungi Kami</li>
       </ul>
-      <div className="flex items-center space-x-4">
-        <button
-          variant="outline"
-          className="flex flex-row rounded-[40px] w-[110px] h-[44px] justify-center items-center text-blue-600 border border-[#B5B5B5]"
-        >
-          <span className="text-[16px] font-inter">Masuk</span>
-          <span className="text-xl">
-            <Icon icon="basil:caret-right-solid" />
-          </span>
-        </button>
-        <button className="flex flex-row justify-center items-center rounded-[40px] w-[110px] h-[44px]  bg-[#2667FF] text-white">
-          <span className="text-[16px] font-inter">Daftar</span>
-          <span className="text-xl">
-            <Icon icon="basil:caret-right-solid" />
-          </span>
-        </button>
-      </div>
+
+      <Link
+        to="/login"
+        variant="outline"
+        className="flex flex-row rounded-[40px] w-[110px] h-[44px] justify-center items-center text-blue-600 border border-[#B5B5B5]"
+      >
+        <span className="text-[16px] font-inter">Masuk</span>
+        <span className="text-xl">
+          <Icon icon="basil:caret-right-solid" />
+        </span>
+      </Link>
     </nav>
   );
 };
